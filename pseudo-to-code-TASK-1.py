ac00d8bcf417c5513eb0ec6f0581f8caec311c96
Philani Mtembu <philanimthembu21@gmail.com>
2020-11-25 17:07:39 +0200

    Added pseudo-to-code-TASK-1.py file with relevent code

---
+++ pseudo-to-code-TASK-1.py
@@ -0,0 +1,8 @@
+x = 0
+y = 1
+print(f"The value of x is: {x} \nThe value of y is: {y}")
+
+#updates the values of x and y
+x = x + 3
+y = y + x
+print(f"The value of x is: {x} \nThe value of y is: {y}")
