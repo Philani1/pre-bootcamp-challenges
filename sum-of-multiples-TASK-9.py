6cceaf5affb487b20fc4edbfb11a8db556619df7
Philani Mtembu <philanimthembu21@gmail.com>
2020-11-26 20:45:20 +0200

    sum-of-multiples-TASK-9.py added with one function that calculates sum of multiples of 3 or 5 below 1000

---
+++ sum-of-multiples-TASK-9.py
@@ -0,0 +1,14 @@
+
+def sum_of_multiples():
+    count = 1
+    sum = 0
+    for i in range(1, 10):
+        if count % 3 == 0:
+            sum += count
+        if count % 5 == 0:
+            sum += count
+        count += 1
+    return sum
+
+som = sum_of_multiples() 
+print(f"The sum of all the multiples of 3 or 5 below 1000 is: {som}")
