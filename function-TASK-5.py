0905a9d972cae734ee04d4de1886f86098d4598e
Philani Mtembu <philanimthembu21@gmail.com>
2020-11-28 15:15:34 +0200

    updated function-TASK-5.py changed changed function parameters from a, b, c to side_a, side_b, side_c

--- function-TASK-5.py
+++ function-TASK-5.py
@@ -1,6 +1,6 @@
-def area(a, b, c):
-    semi_perimetre = (1/2)*(a+b+c)
-    area = (semi_perimetre*(semi_perimetre-a)*(semi_perimetre-b)*(semi_perimetre-c))**(1/2)
+def area(side_a, side_b, side_c):
+    semi_perimetre = (1/2)*(side_a + side_b + side_c)
+    area = (semi_perimetre * (semi_perimetre - side_a)*(semi_perimetre - side_b)*(semi_perimetre - side_c))**(1/2)
     return area
 
 Area = area(3, 4, 5)
