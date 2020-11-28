4c0b632be47d06b3fffa68fe474a8203ef7b0abb
Philani Mtembu <philanimthembu21@gmail.com>
2020-11-28 15:08:18 +0200

    updated function-TASK-5.py file removed user input and changed variable s to semi_perimetre

--- function-TASK-5.py
+++ function-TASK-5.py
@@ -1,14 +1,7 @@
-a = float(input("Enter the first side length in cm: "))
-b = float(input("Enter the second side length in cm: "))
-c = float(input("Enter the last side length in cm: "))
-
 def area(a, b, c):
-    if(a == b and b == c):
-        area = ((b*b) * (3**(1/2.0)/4))
-    else:
-        s = (1/2)*(a+b+c)
-        area = (s*(s-a)*(s-b)*(s-c))**(1/2)
+    semi_perimetre = (1/2)*(a+b+c)
+    area = (semi_perimetre*(semi_perimetre-a)*(semi_perimetre-b)*(semi_perimetre-c))**(1/2)
     return area
 
-Area = area(a, b, c)
+Area = area(3, 4, 5)
 print(f"Area of a triangle is: {Area} cm squared")
