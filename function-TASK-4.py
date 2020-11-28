ff9f15734ec45b0fa157acdc4c13e65800205427
Philani Mtembu <philanimthembu21@gmail.com>
2020-11-28 16:38:34 +0200

    updated function-TASK4.py removed user input

--- function-TASK-4.py
+++ function-TASK-4.py
@@ -2,7 +2,7 @@ def logic_funct(num1, num2):
     sum = num1 + num2     
     return (num1 == 3 or num2 == 3) and sum % 10 == 3        
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+num1 = int(10)
+num2 = int(3)
 
 print(logic_funct(num1, num2))
