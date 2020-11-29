f28843f3f12abc63426367005739ed321c134e11
Philani Mtembu <philanimthembu21@gmail.com>
2020-11-29 11:55:26 +0200

    changed function name

--- function-TASK-4.py
+++ function-TASK-4.py
@@ -1,8 +1,8 @@
-def logic_funct(num1, num2):
-    sum = num1 + num2     
+def test_number_3(num1, num2):
+    sum = int(num1) + int(num2)     
     return (num1 == 3 or num2 == 3) and sum % 10 == 3        
 
 num1 = int(10)
 num2 = int(3)
 
-print(logic_funct(num1, num2))
+print(test_number_3(num1, num2))
