abbe3c06a2ddf53e00cf4f0f63bb2620ae0fbac1
Philani Mtembu <philanimthembu21@gmail.com>
2020-11-29 12:09:01 +0200

    fixed naming conventions, changed function name and file name

--- calculate_area-TASK-5.py
+++ calculate_area-TASK-5.py
@@ -1,7 +1,9 @@
-def area(side_a, side_b, side_c):
+def calculate_area(side_a, side_b, side_c):
     semi_perimetre = (1/2)*(side_a + side_b + side_c)
+
     area = (semi_perimetre * (semi_perimetre - side_a)*(semi_perimetre - side_b)*(semi_perimetre - side_c))**(1/2)
+
     return area
 
-Area = area(3, 4, 5)
-print(f"Area of a triangle is: {Area} cm squared")
+area = calculate_area(3, 4, 5)
+print(f"Area of a triangle is: {area} cm squared")
