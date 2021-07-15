a6b67a03747e59b09b29f050536cbd53df21fb92
Philani Mtembu <philanimthembu21@gmail.com>
2021-07-15 13:53:41 +0200

    fixed formatting using black formatter

--- pseudo-to-code-TASK-2.py
+++ pseudo-to-code-TASK-2.py
@@ -1,10 +1,12 @@
 x = 1 + 1 * 2
 y = (1 + 1) * 2
-z = 1 + ( 1 * 2 )
-a =  1 + 1 * 2 / 2
-b =  (1 + 1 * 2 ) /  2
-print (f"The value of x is: {x} \n"\
-    f"The value of y is: {y}\n"\
-    f"The value of z is: {z}\n"\
-    f"The value of a is: {a}\n"\
-    f"The value of b is: {b}\n")
+z = 1 + (1 * 2)
+a = 1 + 1 * 2 / 2
+b = (1 + 1 * 2) / 2
+print(
+    f"The value of x is: {x} \n"
+    f"The value of y is: {y}\n"
+    f"The value of z is: {z}\n"
+    f"The value of a is: {a}\n"
+    f"The value of b is: {b}"
+)
