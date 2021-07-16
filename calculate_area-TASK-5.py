04b6f4c0e1d7054644ac67f92b3175f64a9dc985
Philani Mtembu <philanimthembu21@gmail.com>
2021-07-16 08:08:17 +0200

    formatted using black formatter, added __name__ == __main

--- calculate_area-TASK-5.py
+++ calculate_area-TASK-5.py
@@ -1,9 +1,14 @@
 def calculate_area(side_a, side_b, side_c):
-    semi_perimetre = (1/2)*(side_a + side_b + side_c)
+    semi_perimetre = (1 / 2) * (side_a + side_b + side_c)
+    area = (
+        semi_perimetre
+        * (semi_perimetre - side_a)
+        * (semi_perimetre - side_b)
+        * (semi_perimetre - side_c)
+    ) ** (1 / 2)
 
-    area = (semi_perimetre * (semi_perimetre - side_a)*(semi_perimetre - side_b)*(semi_perimetre - side_c))**(1/2)
+    print(f"Area of a triangle is: {area} cm squared")
 
-    return area
 
-area = calculate_area(3, 4, 5)
-print(f"Area of a triangle is: {area} cm squared")
+if __name__ == "__main__":
+    area = calculate_area(3, 4, 5)
