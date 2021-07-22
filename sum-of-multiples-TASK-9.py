d51ab27fcecfdd42e2b1300d5eb68d9f21e7b7c9
Philani Mtembu <philanimthembu21@gmail.com>
2021-07-22 14:59:03 +0200

    added __name__ == __main__, code cleaned

--- sum-of-multiples-TASK-9.py
+++ sum-of-multiples-TASK-9.py
@@ -1,14 +1,9 @@
-
 def sum_of_multiples():
-    count = 1
     sum = 0
-    for i in range(1, 1000):
-        if count % 3 == 0:
+    for count in range(0, 1000):
+        if count % 3 == 0 or count % 5 == 0:
             sum += count
-        if count % 5 == 0:
-            sum += count
-        count += 1
     return sum
 
-som = sum_of_multiples() 
-print(f"The sum of all the multiples of 3 or 5 below 1000 is: {som}")
+if __name__ == "__main__":
+    print(f"The sum of all the multiples of 3 or 5 below 1000 is: {sum_of_multiples()}")
