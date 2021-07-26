ce94e77aea22b17163df71c9989fb363e9f37653
Philani Mtembu <philanimthembu21@gmail.com>
2021-07-26 22:31:11 +0200

    changed file name, updated the code to print output

---
+++ print_vowel_09.py
@@ -0,0 +1,11 @@
+def print_vowel(any_string):
+    output = ""
+    for vowel in any_string.lower():
+        if vowel in "aeiou" and vowel:
+            output += vowel
+    output = "".join(sorted(set(output), key=output.index))
+    print(output)
+
+
+if __name__ == "__main__":
+    print_vowel("Umuzi")
