a503c5538fcc22f8be24ad311feaa067445b85f8
Philani <41421055+Philani1@users.noreply.github.com>
2021-07-26 23:19:47 +0200

    deleted file belongs to L1 coding challenges

--- sum-of-multiples-TASK-9.py
+++
@@ -1,9 +0,0 @@
-def sum_of_multiples():
-    sum = 0
-    for count in range(0, 1000):
-        if count % 3 == 0 or count % 5 == 0:
-            sum += count
-    return sum
-
-if __name__ == "__main__":
-    print(f"The sum of all the multiples of 3 or 5 below 1000 is: {sum_of_multiples()}")
